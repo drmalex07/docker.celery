fc34219af60d0b08e41b0e04f5e15d3cd14a11c4
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2015-10-15 14:52:04 +0300

    Add an example with child image supplying tasks from a foo package

---
+++ examples/foo-tasks/foo/foo/tasks.py
@@ -0,0 +1,47 @@
+from celery import Celery
+from celery.task import task, subtask
+from celery.utils.log import get_task_logger
+
+import time
+
+log1 = get_task_logger(__name__)
+
+@task
+def add(x, y):
+    log1.info ('Adding %.2f and %.2f' %(x,y))
+    # calculate and return 
+    return x + y
+
+@task
+def adda(*args):
+    '''A simple task adding all arguments'''
+    log1.info('Adding numbers ' + repr(args))
+    return sum(args)
+
+@task
+def addi(it):
+    '''A simple task adding all items from iterable'''
+    log1.info('Adding numbers ' + repr(it))
+    return sum(it)
+
+@task
+def mul(x, y):
+    log1.info('Multiplying %.2f and %.2f' %(x,y))
+    return x * y
+
+@task
+def waste_time(n=12, callback=None): 
+    '''Emulate a long-running task'''
+
+    for i in range(0, n):
+        log1.info('Wasting some time (%d/%d)' % (i, n))
+        time.sleep(5)
+    
+    if callback:
+        log1.info('Finished task: About to invoke %r' % (callback))
+        subtask(callback).delay()
+    else:
+        log1.info('Finished task')
+    
+    return {'status': 'wasted'}
+
---
+++ examples/foo-tasks/foo/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='foo',
+    version='0.1',
+    description='A foo package that defines Celery tasks',
+    url='http://github.com/drmalex07/foo',
+    author='Michail Alexakis',
+    author_email='malex@example.com',
+    license='MIT',
+    packages=['foo'],
+    zip_safe=False)
